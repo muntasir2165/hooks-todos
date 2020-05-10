8101005f9275052c9b743630b212eec11931a877
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-10 04:34:06 -0700

    video-37 delete request to remove todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import axios from 'axios';
 import TodosContext from '../context';
 
 export default function TodoList() {
@@ -37,7 +38,12 @@ export default function TodoList() {
               />
             </button>
             <button
-              onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}
+              onClick={async () => {
+                await axios.delete(
+                  `https://hooks-api-master.muntasir2165.now.sh/todos/${todo.id}`
+                );
+                dispatch({ type: 'REMOVE_TODO', payload: todo });
+              }}
             >
               <img
                 src='https://icon.now.sh/delete/8b0000'
