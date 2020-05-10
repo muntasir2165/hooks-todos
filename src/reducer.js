0ff8aa5e589ac48d0c6dee823da4fcf6c4d1a8d0
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-10 00:46:12 -0700

    video-33 updating todos / UPDATE_TODO case

--- src/reducer.js
+++ src/reducer.js
@@ -6,6 +6,8 @@ export default function reducer(state, action) {
       const newTodo = { id: uuidv4(), text: action.payload, complete: false };
       const addedTodos = [...state.todos, newTodo];
       return { ...state, todos: addedTodos };
+    case 'SET_CURRENT_TODO':
+      return { ...state, currentTodo: action.payload };
     case 'TOGGLE_TODO':
       const toggledTodos = state.todos.map((t) =>
         t.id === action.payload.id
@@ -13,6 +15,17 @@ export default function reducer(state, action) {
           : t
       );
       return { ...state, todos: toggledTodos };
+    case 'UPDATE_TODO':
+      const updatedTodo = { ...state.currentTodo, text: action.payload };
+      const updatedTodoIndex = state.todos.findIndex(
+        (t) => t.id === state.currentTodo.id
+      );
+      const updatedTodos = [
+        ...state.todos.slice(0, updatedTodoIndex),
+        updatedTodo,
+        ...state.todos.slice(updatedTodoIndex + 1),
+      ];
+      return { ...state, currentTodo: {}, todos: updatedTodos };
     case 'REMOVE_TODO':
       const filteredTodos = state.todos.filter(
         (t) => t.id !== action.payload.id
