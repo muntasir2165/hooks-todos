c6384c4b0de76a97e40dc20d49e5ded8dd579a51
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-10 03:59:51 -0700

    video-36 creating a custom hook to fetch initial app data

--- src/context.js
+++ src/context.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const TodosContext = React.createContext({
   todos: [
-    { id: 1, text: 'Eat breakfast', complete: false },
-    { id: 2, text: 'Do laundry', complete: false },
-    { id: 3, text: 'Finish project', complete: true },
+    // { id: 1, text: 'Eat breakfast', complete: false },
+    // { id: 2, text: 'Do laundry', complete: false },
+    // { id: 3, text: 'Finish project', complete: true },
   ],
-  currentTodo: {}
+  currentTodo: {},
 });
 
 export default TodosContext;
